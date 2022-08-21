09d6b99fb3c62a816657a3c54c8c6e0c498cd4e8
Amir Hosein Nazari <czhablog@gmail.com>
2022-08-21 11:38:59 +0400

    Implemented new twitter v2
    fetched the tweets via the desired keyword

--- twitter_api.py
+++ twitter_api.py
@@ -1,8 +1,11 @@
+import requests
 import tweepy
 import configparser
+import os
+import json
 
 # read configs
-config = configparser.ConfigParser()
+config = configparser.RawConfigParser()
 config.read('config.ini')
 
 api_key = config['twitter']['api_key']
@@ -10,10 +13,39 @@ api_key_secret = config['twitter']['api_key_secret']
 
 access_token = config['twitter']['access_token']
 access_token_secret = config['twitter']['access_token_secret']
+bearer_token = config['twitter']['bearer_token']
 
-# authentication
-auth = tweepy.OAuthHandler(api_key, api_key_secret)
-auth.set_access_token(access_token, access_token_secret)
+search_url = "https://api.twitter.com/2/tweets/search/recent"
 
-api = tweepy.API(auth)
-hellow world
+# Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
+# expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
+query_params = {'query': 'ワクチン'}
+
+
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
+
+    r.headers["Authorization"] = f"Bearer {bearer_token}"
+    r.headers["User-Agent"] = "v2RecentSearchPython"
+    return r
+
+
+def connect_to_endpoint(url, params):
+    response = requests.get(url, auth=bearer_oauth, params=params)
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    return response.json()
+
+
+def main():
+    json_response = connect_to_endpoint(search_url, query_params)
+    print(json_response)
+    for item in json_response['data']:
+        print(item['text'].encode('utf-16', 'surrogatepass').decode('utf-16'))
+
+
+if __name__ == "__main__":
+    main()
